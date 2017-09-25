57d34e2d8188f48b0570e95e482dd93381ca8719
dong <dong@dong-PC>
2017-09-25 22:47:29 +0900

    트랜잭션수정4

--- src/main/java/transaction/SpringMybatisTest_Transaction.java
+++ src/main/java/transaction/SpringMybatisTest_Transaction.java
@@ -7,18 +7,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringMybatisTest_Transaction {
 public static void main(String[] args) {
-//1. spring bean ���� ���� �о��
+
 	ApplicationContext factory
 	= new ClassPathXmlApplicationContext
 	("transaction/springbean_transaction.xml");
 	
-//2. ���񽺰�ü ���Ͷ�
-//spring-service�������̽�/serviceImpl���/dao/vo/db	
+	
 	EmpService service = (EmpService)factory.getBean("empService");
-//3.���񽺰�ü : ��� ��� ��ȸ mybatis ����
+
 	List<EmpVO> list = service.getEmpList();
-//4.���	
-	System.out.println("test333");
+
+	System.out.println("test444");
 	for(int i = 0; i < list.size(); i++){
 		EmpVO vo2 = list.get(i);
 		System.out.println(vo2.getEmployee_id() + " / " + vo2.getLast_name()
